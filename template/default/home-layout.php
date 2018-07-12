1383ae0560cd4211d01cf2adca96314136024639
macq@MacQs-MacBook.(none) <macq@MacQs-MacBook.local>
2018-07-12 20:39:28 +0700

    Create Project

--- template/default/home-layout.php
+++ template/default/home-layout.php
@@ -3,17 +3,20 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="description" content="Learn how to include Bootstrap in your project using Webpack 3.">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Jekyll v3.7.0">
     <link rel="icon" href="<?php echo resource_url("favicon.ico");?>">
     <title><?php echo $header["title"];?></title>
     <script src="<?php echo resource_url("js/apps.js");?>"></script>
+    <link rel="stylesheet" href="<?php echo resource_url("bootstrap/css/bootstrap-extension.css");?>">
+    <script src="<?php echo resource_url("bootstrap/js/bootstrap-extension.js");?>"></script>
+    <link rel="stylesheet" href="<?php echo template_url("css/ui.css");?>">
     <link rel="stylesheet" href="<?php echo template_url("css/style.css");?>">
     <script src="<?php echo template_url("js/main.js");?>"></script>
 
-    <link rel="stylesheet" href="<?php echo resource_url("bootstrap/css/bootstrap-extension.css");?>">
-    <script src="<?php echo resource_url("bootstrap/js/bootstrap-extension.js");?>"></script>
+    
 
   </head>
     <body>
