5cc9f3c620d70f460fa95710bc60275a5680da20
macq@MacQs-MacBook.(none) <macq@MacQs-MacBook.local>
2018-07-15 17:40:30 +0700

    Revert "Merge branch 'master' of https://github.com/roller-project/website"
    This reverts commit b2e1a73e9941c507668c8dfca1d7482c9eaa699e, reversing
    changes made to 6a061c16caab55bb28440077f9059cc13a191adb.

--- resource/apps/home.php
+++ resource/apps/home.php
@@ -50,11 +50,7 @@
         <!-- End SVG Shapes v4 -->
 
         <!-- Sidebar -->
-<<<<<<< HEAD
         <div class="col-lg-4 u-bg-img-hero min-height-100vh--lg position-absolute-top-right-0--lg px-lg-0 u-space-2" style="background-image: url(https://bitcoinist.com/wp-content/uploads/2017/12/ss-crypto-future.jpg);">
-=======
-        <div class="col-lg-4 u-bg-img-hero min-height-100vh--lg position-absolute-top-right-0--lg px-lg-0 u-space-2" style="background-image: url(<?php echo store_url("uploads/bg6.jpg");?>);">
->>>>>>> 12a81715a88d2eb7edc99caf071c84114c370fd9
           <!-- Fancybox -->
           <div class="text-center text-lg-left mb-3 mb-lg-0">
             <a class="js-fancybox u-media-player u-media-player--left-minus-50x-top-50x-lg mb-4" href="javascript:;" data-src="//vimeo.com/167434033" data-speed="700" data-animate-in="zoomIn" data-animate-out="zoomOut" data-caption="Front - Responsive Website Template">
@@ -551,79 +547,6 @@
 
 
 
-
-<div class="container">
-      <div class="row justify-content-lg-between align-items-center">
-        <div class="col-md-6 col-lg-5 order-md-2 mb-7 mb-md-0">
-          <div class="pl-md-4">
-            <!-- Info -->
-            <div class="media mb-7">
-              <div class="d-flex mr-3">
-                <span class="u-icon u-icon-success--air u-icon--sm rounded-circle">
-                  <span class="u-icon__inner font-weight-bold">C.</span>
-                </span>
-              </div>
-              <div class="media-body">
-                <h3 class="h5 text-success">Advanced HTML/CSS editing</h3>
-                <p>With Front, you or your team can modify any aspect of your website or pages with HTML/CSS.</p>
-              </div>
-            </div>
-            <!-- End Info -->
-
-            <!-- Info -->
-            <div class="media mb-7">
-              <div class="d-flex mr-3">
-                <span class="u-icon u-icon-success--air u-icon--sm rounded-circle">
-                  <span class="u-icon__inner font-weight-bold">D.</span>
-                </span>
-              </div>
-              <div class="media-body">
-                <h3 class="h5 text-success">Fully optimized for Web &amp; Mobile</h3>
-                <p>Your website is fully responsive so visitors can view your content from their choice of device.</p>
-              </div>
-            </div>
-            <!-- End Info -->
-
-            <a class="btn btn-success u-btn-success u-btn-wide u-btn-pill transition-3d-hover" href="https://themes.getbootstrap.com/product/front-multipurpose-responsive-template/" target="_blank">Purchase Now <span class="fa fa-angle-right ml-2"></span></a>
-          </div>
-        </div>
-
-        <div class="col-md-6 order-md-1">
-          <!-- SVG Mockup -->
-          <figure>
-            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 614.8 747.7" style="enable-background:new 0 0 614.8 747.7;" xml:space="preserve">
-              <path class="u-fill-success" opacity=".05" d="M428.5,138.9C489.6,144.5,545.6,176,581,226c54.3,76.8,67.1,204.7-193.8,371.7c-411.5,263.5-387.9,6-382.7-30.6
-                c0.5-3.6,0.9-7.2,1.3-10.7l22.1-241.9c10.6-116.2,113.5-201.8,229.8-191.1L428.5,138.9z"></path>
-              <g>
-                <defs>
-                  <path id="mockup3" d="M446.2,729.7L446.2,729.7C310.4,782.7,156,714.9,103,579.1L18,361.2C-34.9,225.4,32.8,71,168.6,18l0,0
-                    c135.8-53,290.2,14.8,343.2,150.6l85,217.9C649.8,522.3,582,676.7,446.2,729.7z"></path>
-                </defs>
-                <clipPath id="mockup4">
-                  <use xlink:href="#mockup3" style="overflow:visible;"></use>
-                </clipPath>
-                <g style="clip-path:url(#mockup4);">
-                  <!-- Apply your (615px width to 750px height) image here -->
-                  <image style="overflow:visible;" width="615" height="750" xlink:href="https://htmlstream.com/preview/front-v1.2/assets/img/615x750/img2.jpg" transform="matrix(1 0 0 1 -3.177470e-02 -0.9532)"></image>
-                </g>
-              </g>
-              <g>
-                <circle class="u-fill-danger" cx="488.6" cy="693.8" r="16.3"></circle>
-                <circle class="u-fill-success" cx="482" cy="632.6" r="10.6"></circle>
-                <circle class="u-fill-primary" cx="537.8" cy="655.9" r="21.6"></circle>
-                <circle class="u-fill-warning" cx="576" cy="708.1" r="3.9"></circle>
-              </g>
-            </svg>
-          </figure>
-          <!-- End SVG Mockup -->
-        </div>
-      </div>
-    </div>
-
-
-
-
-    
 
 
 
