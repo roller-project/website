dbc5dc47fd24cc7b2efb1a75061d1f3dd4e5a12c
Roller Davic <rollerdavic@gmail.com>
2018-07-16 11:51:43 +0700

    Create Project

--- template/admin/home-layout.php
+++ template/admin/home-layout.php
@@ -55,10 +55,9 @@
 
               
             </ul>
-            <form class="form-inline my-2 my-lg-0">
-              <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
+            <div class="form-inline my-2 my-lg-0">
+              Language : <a href="?language=english">English</a> | <a href="?language=vietnam">Vietnam</a> | <a href="?language=china">China</a> | <a href="?language=russia">Russia</a> | <a href="?language=korea">Korea</a>
+            </div>
           </div>
         </nav>
 
