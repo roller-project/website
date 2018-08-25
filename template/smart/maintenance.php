59c57b1b2323fe9688769f3b1dc1e9330d2fae3f
Roller Davic <rollerdavic@gmail.com>
2018-08-25 16:12:16 +0700

    Update

---
+++ template/smart/maintenance.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Learn how to include Bootstrap in your project using Webpack 3.">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.7.0">
+    <link rel="icon" href="<?php echo resource_url("favicon.ico");?>">
+    <title>Roller Platform</title>
+    <script src="<?php echo resource_url("js/apps.js");?>"></script>
+    <link rel="stylesheet" href="<?php echo template_url("css/style.css");?>">
+    <script src="<?php echo template_url("js/main.js");?>"></script>
+  </head>
+    <body>
+      <main id="content">
+          <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+            <h1 class="display-4">Maintenace Service</h1>
+            <p class="lead">We update big system. All back online 27/08/2018. You can user cms <a href="http://cms.roller.today">http://cms.roller.today</a></p>
+          </div>
+      </main>
+    
+    </body>
+</html>
