e8ed7cedbbcdebc6593a16c5c6665f550da9c359
Roller Davic <rollerdavic@gmail.com>
2018-08-28 18:55:51 +0700

    up

--- language/english/globals_lang.php
+++ language/english/globals_lang.php
@@ -1,124 +1,121 @@
 <?php
-$lang["home"]	= "Home page";
+$lang["home"]	= "Home";
 $lang["account"]	=	"Account";
-$lang["error"] = "Lỗi";
-$lang["success"]	= "Hoàn thành";
-$lang["warning"]	=	"Cảnh bảo";
-$lang["account_ready"]	=	"Email đăng ký đã tồn tại";
-$lang["success_register"]	= "Tài khoản được tạo. Bạn có thể đăng nhập";
-$lang["success_login"]		=	"Đăng nhập thành công";
+$lang["error"] = "Error";
+$lang["success"]	= "Complete";
+$lang["warning"]	=	"Warning";
+$lang["account_ready"]	=	"Your email already exists";
+$lang["success_register"]	= "Account created. You can login";
+$lang["success_login"]		=	"Login successful";
 
-$lang["success_update"]		=	"Cập nhập thành công";
-$lang["error_update"]		=	"Lỗi cập nhập vui lòng kiểm tra lại";
+$lang["success_update"]		=	"Update successful";
+$lang["error_update"]		=	"Update error, please check again";
 
-$lang["success_insert"]		=	"Dữ liệu được thêm thành công";
-$lang["error_insert"]		=	"Lỗi khi thêm dữ liệu";
-$lang["support_customer"] = 	"Support Customer";
-$lang["chanel_customer"]	=	"Channel";
-$lang["roadmap"]			=	"Roadmap";
-$lang["more"]				=	"More.";
+$lang["success_insert"]		=	"The data was successfully added";
+$lang["error_insert"]		=	"Error adding data";
+$lang["support_customer"] = 	"User support";
+$lang["chanel_customer"]	=	"Support channels";
+$lang["roadmap"]			=	"Development plan";
+$lang["more"]				=	"Read more.";
+$lang["enter_email"]		=	"Type your email";
+$lang["btn_join"]			=	"Join now";
 
-$lang["enter_email"]		=	"Enter your email";
-$lang["btn_join"]			=	"Join";
-
+$lang["help"] = "Guides";
+$lang["faq"] = "Q&A";
+$lang["contacts"] = "Contact";
+$lang["application"]	=	"Apps";
+$lang["ecosystem"]	=	"Roller ecosystem";
+$lang["help_customer"] = "Community support";
+$lang["report_project"] = "Financial report";
+$lang["explorer"] = "Distributed Lookup";
+$lang["aboutus"] = "About us";
+$lang["development_plan"] = "Development plan";
+$lang["about_community"] = "About us";
 
 /*
 Language Text page
 */
 
 
-$lang["partners"] = "Application development partners";
-$lang["partners_description"] = "We are pleasure with all contributions and development projects to be completed to reach the goal that is a community project.";
-$lang["joingroup_title"] = "Easy with Roller Member";
-$lang["joingroup_description"] = "Enter you email";
-$lang["help_list_exchange"] = "";
+$lang["partners"] = "Application development partner";
+$lang["partners_description"] = "We welcome all contributions and project development to achieve our goal of being a community project.";
+$lang["joingroup_title"] = "Simple way";
+$lang["joingroup_description"] = "Enter email to become a member of our community. The latest information from us will be delivered to you immediately.";
+$lang["help_list_exchange"] = 'Our development processes depend on the cost and technology contributions of the community. Donations please send to <a href="https://www.blockchain.com/btc/address/18UDydoLS6eckX2aspZLWUWngKkZahWPkK" target="_bank">18UDydoLS6eckX2aspZLWUWngKkZahWPkK</a>';
 
 
-
-$lang["help"] = "Documentation";
-$lang["faq"] = "Faq's";
-$lang["contacts"] = "Contact";
-$lang["application"]	=	"Application";
-$lang["help_customer"] = "Community & Help";
-$lang["ecosystem"]	=	"Roller Ecosystem";
-$lang["report_project"] = "Financial Report";
-$lang["explorer"] = "Block Explorer";
-$lang["aboutus"] = "Community";
-$lang["development_plan"] = "Development plan";
-$lang["about_community"] = "About Community";
 /*
 New Language
 */
-
 
 $lang["info_name"] = "Name";
 $lang["info_symbol"] = "Symbol";
 $lang["info_type"] = "Type";
 $lang["info_algorithm"] = "Algorithm";
-$lang["info_target"] = "Target";
-$lang["info_rewand"] = "Rewand";
-$lang["info_total"] = "Total";
-$lang["info_ico"] = "No ICO";
-$lang["info_premice"] = "No Premice";
+$lang["info_target"] = "Time";
+$lang["info_rewand"] = "The coin incurred";
+$lang["info_total"] = "General publication";
+$lang["info_ico"] = "Do not sell ICO";
+$lang["info_premice"] = "No create Premice\'s coin";
 
+$lang["roadmap"] = "Route";
+$lang["roadmap_title"] = "Development Roadmap";
 
-$lang["roadmap"] = "Roadmap";
-$lang["roadmap_title"] = "Roadmap Timeline";
-$lang["roller_infomation"] = "Roller Infomation";
-$lang["timefork"] = "Time Fork";
-$lang["exchange"] = "Exchange";
+$lang["roller_infomation"] = "Information Roller";
+$lang["timefork"] = "Event Time";
+
+$lang["exchange"] = "Exchanges";
 $lang["wallet"] = "Wallet";
-$lang["mining"] = "Mining";
-$lang["mining_title"] = "Roller Infomation";
+$lang["mining"] = "Exploited";
+$lang["mining_title"] = "Specifications";
 $lang["mining_text_1"] = 'To exploit Roller you need a wallet for you. You can sign up for a free wallet online and use it at <a href="http://cms.roller.today" target="_bank">https://cms.roller.today</a><br>
   Then use the usual ethereum mining software like claymore ... to connect to the digging pool. You can refer to the roller support roller underneath';
-$lang["mining_text_2"] = 'In addition, you can exploit and own ROL via Masternode, Airdrop and other ROL-enabled applications. You can refer to the application process of ROL <a href="/blogs.html">here.</a>';
+$lang["mining_text_2"] = 'In addition, you can exploit and own ROL via Masternode, Airdrop and other ROL-enabled applications. You can refer to the application process of ROL at <a href="/blogs.html">here.</a>';
 
-$lang["team"] = "Team";
-$lang["team_title"] = "Team Development";
+$lang["team"] = "Development team";
+$lang["team_title"] = "Co-developed Roller";
 $lang["report"] = "Report";
 $lang["blogs"] = "Blogs";
-$lang["blogs_title"] = "New & Events Roller";
-$lang["platformcontact"] = "Contacts Roller Platform";
+$lang["blogs_title"] = "News & Activities Roller";
+$lang["platformcontact"] = "Contact with Roller Platform";
 
-$lang["wallet_application"] = "Wallet Application";
+$lang["wallet_application"] = "Wallet management software";
 
 $lang["developteam"] = [
-	"Executive development of the Roller system",
-	"Consulting, Development operating Blockchain system",
-	"Executive, Development Exchange Roller",
-	"Executive, Development Masternode",
-	"Smart Contracts Development",
-	"Application Development"
+	"Executive Development of Global Roller System",
+	"Development consultant for the operation of the Blockchain system",
+	"Executive Exchange Roller Development",
+	"Executive Development Masternode",
+	"Developing Smart Contracts",
+	"Executive Development Application"
 ];
-$lang["developteam_more"] = 'And some co-developed the blockchain system. If you have a blockchaign application idea in your life, please email us via <a href="mainto:rollerdavic@gmail.com?subject=development"> rollerdavic@gmail.com </a>. >. We always respect all your ideas for development.';
+$lang["developteam_more"] = 'And some co-developed the blockchain system. If you have a blockchaign application idea in your life, please email us via <a href="mainto:rollerdavic@gmail.com?subject=development"> rollerdavic@gmail.com </a>. >. We always respect your comments, contribute and development';
 
 $lang["roadmap_timeline"] = [
-	"<b>Launch the system</b><br>Launch the Blockchain system, build relevant data analysis systems, run the ( Maternode )",
-	"<b>Applying Applications to Blockchain </b> <br> Bringing Applications into Use in Roller Systems As the foundation for CMS-based operating systems",
-	"<b>List Exchange</b><br>Develop marketing and set up liquidity transactions for ROL. This is the development stage of attracting investment",
-	"<b>Ecosystem Development</b><br>Focus on Developing Blockchain-based Applications to Attract Roller Platform Investment",
-	"<b>Smart Contracts Development</b><br>The contract application development phase is intended to give the user exclusive blockchain application features",
-	"<b>Development of Connected Services</b><br>3rd Generation Connected Technology Services Phase. As Automation, Security, IoT ..."
+	"<b> Launch the system </ b> - Launch the Blockchain system, build relevant data analysis systems, run the Maternode"
+	"<b> Applying Applications to Blockchain </ b> <br> Bringing Applications into Use in Roller Systems </ b>"
+	"<b> List Exchange </ b> - Marketing development and set-up liquidity transactions for ROL. Sync System will be processed"
+	"<b> Ecosystem Development </ b> - Focus on Developing Blockchain-based Applications to Attract Roller Platform Investment,"
+	"<b> Smart Contracts Development </ b> - The contract application development phase is intended to give the user exclusive blockchain application features,"
+	"<b> Development of Connected Services </ b> - 3rd Generation Connected Technology Services Phase. As Automation, Security, IoT ... "
 ];
-
 
 $lang["slider"] = [
 	[
 	"image" => template_url("image/banner.jpg"),
 	"caption" => '<h3>Roller Platform</h3>
-          <p>Roller Platform là một hệ sinh thái tiền mã hóa mã nguồn mở phi tập trung, dựa trên nền tảng của Ethereum với ký hiệu của riêng là "ROL". <br>Mục tiêu chính của Roller là nghiên cứu, phát triển một cơ chế tự duy trì, tự quản lí, tự tài trợ hệ sinh thái blockchain và môi trường phát triển ứng dụng của blockchain. </p>'
+          <p>Roller Platform is an coin ecosystem from the source code open source, based on the Ethereum platform with private symbol is "ROL". <br> The primary key of the Roller is the research, development with an auto mode, automatic manage, automatic blockchain and development environment of blockchain. </p>'
 	],
 	[
 	"image" => template_url("image/banner.jpg"),
 	"caption" => '<h3>Application</h3>
-            <p>Với các ứng dụng thiết thực từ Smart Contracts như. Tech Document, Share Contracts, ANN, Miner Controller, Security Web 4.0, IoT Services ... Roller sẽ mang đến cho người dùng cuối tiếp cận ứng dụng công nghệ trong thời kỳ công nghiệp 4.0</p>'
+            <p>With practical applications from Smart Contracts like: Tech Document, Share Contracts, ANN, Miner Controller, Security Web 4.0, IoT Services ... Roller will bring end-users access to technology applications in the 4.0</p>'
 	],
 	[
 	"image" => template_url("image/banner.jpg"),
 	"caption" => '<h3>Development</h3>
-          <p>Roller Platform được phát triển bởi các lập trình viên Việt Nam. Với các hệ thống liền mạch như Exchange, CMS, Masternode, etc....<br>
-            Với hệ thống CMS có nhiều Services được cung cấp miễn phí cho người dùng trên các ứng dụng của Roller. Chúng mang đến một cách nhìn tổng quan và thiết thực cuộc sống
+          <p>Roller Platform is developed by Vietnamese programmers. With seamless systems like Exchange, CMS, Masternode, etc<br>
+            With CMS, many services are provided free for users on Roller applications. They provide an overview and practical life
           </p>'
 	]
 
