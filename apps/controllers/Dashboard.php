8ce344ba321a6508f149846832d3904a7230272e
Roller Davic <rollerdavic@gmail.com>
2018-09-12 01:12:24 +0700

    up

--- apps/controllers/Dashboard.php
+++ apps/controllers/Dashboard.php
@@ -85,7 +85,7 @@ class Dashboard extends Frontend {
 
 		$pool[] = [
 			"name" => "Sexy Pool",
-			"url"	=>	"https://rol.pool.sexy"
+			"url"	=>	"https://mining-rol.pool.sexy/"
 		];
 
 		$pool[] = [
